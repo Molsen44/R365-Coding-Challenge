b8406466d373c5b99918ec023b60a847a29c6e68
molsen44 <molsen44@gmail.com>
2020-01-05 17:17:22 -0600

    Uploading APP js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Challenge1 from "./challenge1";
+import Challenge2 from "./challenge2";
+import Challenge3 from "./challenge3";
+import Challenge4 from "./challenge4";
+import Challenge5 from "./challenge5";
+import Challenge6 from "./challenge6";
+import Challenge7 from "./challenge7";
+import Challenge8 from "./challenge8";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app__header">
+        <h1>
+          <span className="app__title">Restaurant365</span> Calculator Challenge
+        </h1>
       </header>
+      <div className="app__content">
+        <Challenge1 />
+        <Challenge2 />
+        <Challenge3 />
+        <Challenge4 />
+        <Challenge5 />
+        <Challenge6 />
+        <Challenge7 />
+        <Challenge8 />
+      </div>
     </div>
   );
 }
