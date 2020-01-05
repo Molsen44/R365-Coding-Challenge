705161f2b7c59ec06c28816b565e37abbc62f01e
molsen44 <molsen44@gmail.com>
2020-01-05 14:17:30 -0600

    Uploading Chall4

---
+++ src/Challenges/Challenes4/Chall4.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Challenge4 from "./";
+import { render, fireEvent } from "@testing-library/react";
+
+test("Deny Negative numbers", () => {
+  const { getByTestId } = render(<Challenge4 />);
+
+  const inField = getByTestId("input");
+  const outField = getByTestId("output");
+  const subinfo = getByTestId("submit");
+
+  fireEvent.change(inField, {
+    target: { value: "1\n3, -5" }
+  });
+
+  fireEvent.click(subinfo);
+
+  expect(outField.textContent).toBe(
+    "Error: Negative Numbers Included in Input: -10,-12"
+  );
+});
