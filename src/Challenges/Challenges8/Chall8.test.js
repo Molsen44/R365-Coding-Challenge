593cdb5ccee8a7967d1a02a7eecf8438e76269a9
molsen44 <molsen44@gmail.com>
2020-01-05 17:16:18 -0600

    Compelted testing.

--- src/Challenges/Challenges8/Chall8.test.js
+++ src/Challenges/Challenges8/Chall8.test.js
@@ -5,57 +5,57 @@ import { render, fireEvent } from "@testing-library/react";
 test("Incorrect Delimiter Format", async () => {
   const { getByTestId } = render(<Challenge8 />);
 
-  const inputField = getByTestId("input");
-  const outputField = getByTestId("output-field");
-  const submitButton = getByTestId("submit-button");
+  const inField = getByTestId("input");
+  const outField = getByTestId("output");
+  const subinfo = getByTestId("submit");
 
-  fireEvent.change(inputField, {
+  fireEvent.change(inField, {
     target: { value: "//#2#5" }
   });
 
-  await fireEvent.click(submitButton);
-  expect(outputField.textContent).toBe("0");
+  await fireEvent.click(subinfo);
+  expect(outField.textContent).toBe("0");
 
-  fireEvent.change(inputField, {
+  fireEvent.change(inField, {
     target: { value: "//##\n2#ff#100" }
   });
 
-  await fireEvent.click(submitButton);
-  expect(outputField.textContent).toBe("0");
+  await fireEvent.click(subinfo);
+  expect(outField.textContent).toBe("0");
 });
 
 test("New Delimiter Format", async () => {
   const { getByTestId } = render(<Challenge8 />);
 
-  const inputField = getByTestId("input");
-  const outputField = getByTestId("output-field");
-  const submitButton = getByTestId("submit-button");
+  const inField = getByTestId("input");
+  const outField = getByTestId("output");
+  const subinfo = getByTestId("submit");
 
-  fireEvent.change(inputField, {
-    target: { value: "//[#]\n2#5" }
+  fireEvent.change(inField, {
+    target: { value: "//[#]\n2#4" }
   });
 
-  await fireEvent.click(submitButton);
-  expect(outputField.textContent).toBe("7");
+  await fireEvent.click(subinfo);
+  expect(outField.textContent).toBe("6");
 
-  fireEvent.change(inputField, {
-    target: { value: "//[@,#]\n2@,#ff@,#100" }
+  fireEvent.change(inField, {
+    target: { value: "//[@,#]\n5@,#ff@,#100" }
   });
 
-  await fireEvent.click(submitButton);
-  expect(outputField.textContent).toBe("102");
+  await fireEvent.click(subinfo);
+  expect(outField.textContent).toBe("105");
 
-  fireEvent.change(inputField, {
-    target: { value: "//[***]\n11***22***33" }
+  fireEvent.change(inField, {
+    target: { value: "//[***]\n11***33***44" }
   });
 
-  await fireEvent.click(submitButton);
-  expect(outputField.textContent).toBe("66");
+  await fireEvent.click(subinfo);
+  expect(outField.textContent).toBe("88");
 
-  fireEvent.change(inputField, {
+  fireEvent.change(inField, {
     target: { value: "//[*][!!][r9r]\n11r9r22*hh*33!!44" }
   });
 
-  await fireEvent.click(submitButton);
-  expect(outputField.textContent).toBe("110");
+  await fireEvent.click(subinfo);
+  expect(outField.textContent).toBe("110");
 });
